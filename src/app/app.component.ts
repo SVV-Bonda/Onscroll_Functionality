65f017408b855c30daed3a395b5b9373ba374150
SVV-Bonda <122274519+SVV-Bonda@users.noreply.github.com>
2023-06-19 19:21:53 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component, VERSION } from '@angular/core';
+declare let alertify: any;
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 
-  onScroll(){
-    alert()
+  onScroll() {
+    alertify.success('scrolled');
     // this.result = [...this.result, ...this.mergeArray]; for Merge array values
   }
 }
